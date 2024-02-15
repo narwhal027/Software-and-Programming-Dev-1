117d8361434895234a8975d76bb5ef544f2dd53a
narwhal027 <158131086+narwhal027@users.noreply.github.com>
2024-02-14 23:12:04 -0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,30 @@
+/**
+ * Hello
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        int age = 18; // Creates an variable to store my age value.
+        String me = "My name is Logan Hansen, and I am "; // Creates a variable to store my name and introduction.
+        String dad = "Steven Hansen (Dad)"; // Creates a variable to store my dad's name.
+        String mom = "Ruth Hansen (Mom)"; // Creates a variable to store my mom's name.
+        String bro1 = "Brendan"; // Creates a variable to store one of my brother's names.
+        String sis1 = "Amber"; // Creates a variable to store one of my sister's names.
+        String sis2 = "Ashley"; // Creates a variable to store another of my sister's names.
+        String sis3 = "Kiara"; // Creates a variable to store a third sister's name.
+        String bro2 = "Calvin"; // Creates a variable to store another brother's name.
+        String sis4 = "Hailey"; // Creates a variable to store a 4th sister's name.
+        String bro3 = "Kyran"; // Creates a variable to store a third brother's name.
+        System.out.println("Hello World"); // Prints "Hello World" in the debugger.
+        System.out.println(me + age); // Prints my introduction and age in the debugger.
+        System.out.println(dad); // Prints my dad's name in the debugger.
+        System.out.println(mom); // Prints my mom's name in the debugger.
+        System.out.println(bro1); // Prints my first brother's name in the debugger.
+        System.out.println(sis1); // Prints my first sister's name in the debugger.
+        System.out.println(sis2); // Prints my second sister's name in the debugger.
+        System.out.println(sis3); // Prints my third sister's name in the debugger.
+        System.out.println(bro2); // Prints my second brother's name in the debugger.
+        System.out.println(sis4); // Prints my fourth sister's name in the debugger.
+        System.out.println(bro3); // Prints my third brother's name in the debugger.
+    }
+}
